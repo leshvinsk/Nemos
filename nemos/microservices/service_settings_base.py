91b408d816bb093cc44e424eacce968a72eefaa5
leshvinsk <114550216+leshvinsk@users.noreply.github.com>
2026-04-29 00:10:31 +0800

    Render Update

--- nemos/microservices/service_settings_base.py
+++ nemos/microservices/service_settings_base.py
@@ -1,8 +1,13 @@
+import os
+
 from nemos.settings import *  # noqa: F401,F403
 
 
 MICROSERVICE_REGISTRY = {
-    "user_service": "http://127.0.0.1:8001",
-    "ngo_service": "http://127.0.0.1:8002",
-    "registration_service": "http://127.0.0.1:8003",
+    "user_service": os.environ.get("USER_SERVICE_URL", "http://127.0.0.1:8001").strip(),
+    "ngo_service": os.environ.get("NGO_SERVICE_URL", "http://127.0.0.1:8002").strip(),
+    "registration_service": os.environ.get(
+        "REGISTRATION_SERVICE_URL",
+        "http://127.0.0.1:8003",
+    ).strip(),
 }
